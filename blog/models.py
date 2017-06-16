213e2835cd4b9ae505eb6cb1e178ec63938a9598
wobenwuliang <1217019920@qq.com>
2017-06-16 17:55:23 +0800

    demo

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+
+#自定义管理器
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+#Post 模型
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -33,3 +42,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args =[self.publish.year,
+                              self.publish.strftime('%m'),
+                              self.publish.strftime('%d'),
+                              self.slug])
